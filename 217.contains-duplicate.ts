3e201bb61e2e0e21bad3004d85c85698ab0b38b5
Yoshiyuki Nakano <syxqh349ysykn@gmail.com>
2023-05-27 10:00:37 +0900

    add easy 217

---
+++ 217.contains-duplicate.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=217 lang=typescript
+ *
+ * [217] Contains Duplicate
+ */
+
+// @lc code=start
+function containsDuplicate(nums: number[]): boolean {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+// @lc code=end
