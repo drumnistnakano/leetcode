f23b16f9fef9df7f4f3656d95cba1df04f19d4c3
drumnistnakano <syxqh349ysykn@gmail.com>
2024-01-06 23:11:52 +0900

    739. Daily Temperatures

--- python/739.daily-temperatures.py
+++ python/739.daily-temperatures.py
@@ -7,6 +7,15 @@
 # @lc code=start
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        
+        result = [0] * len(temperatures)
+        stack = []
+
+        for index, temperature in enumerate(temperatures):
+            while stack and temperature > stack[-1][0]:
+                stack_temperature, stack_index = stack.pop()
+                result[stack_index] = index - stack_index
+            stack.append([temperature, index])
+
+        return result
 # @lc code=end
 
