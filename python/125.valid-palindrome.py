609064d62aed478f820214dfd44a6c9cca5700d4
drumnistnakano <syxqh349ysykn@gmail.com>
2024-01-12 22:07:05 +0900

    125. Valid Palindrome

--- python/125.valid-palindrome.py
+++ python/125.valid-palindrome.py
@@ -7,6 +7,7 @@
 # @lc code=start
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
+        str_array = re.findall('[a-zA-Z0-9]', s.lower())
+        return str_array == list(reversed(str_array))
 # @lc code=end
 
