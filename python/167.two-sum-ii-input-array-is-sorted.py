35298eb6eed9c63245fa75a65cfb2b99fc27c604
drumnistnakano <syxqh349ysykn@gmail.com>
2024-01-14 08:46:18 +0900

    167. Two Sum II - Input Array Is Sorted

--- python/167.two-sum-ii-input-array-is-sorted.py
+++ python/167.two-sum-ii-input-array-is-sorted.py
@@ -7,6 +7,15 @@
 # @lc code=start
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
+        l, r = 0, len(sum) - 1
+
+        while l < r:
+            sum = numbers[l] + numbers[r]
+            if sum > target:
+                r -= 1
+            elif sum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
 # @lc code=end
 
